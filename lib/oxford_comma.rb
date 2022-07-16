59c56db62ecf224f0db956100e6d09a4f0547c45
Lian <biakhong@gmail.com>
2022-07-16 00:02:59 -0400

    COmpleted Assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+    if array.length >2
+        last_str = array[-1]
+        result = array[0..-2].join(", ")
+        result<<(", and #{last_str}")
+    elsif  array.length == 2
+        result = array[0] + " and " + array[1]
+
+    else
+        array.join
+    end
 
 end
+puts oxford_comma(["fiddleheads", "okra", "kohlrabi"])
